edd71ef0ad51d862c4f6fbede95b1a82a10cc55b
Juzet Arana <arana.juzet@gmail.com>
2015-05-07 11:00:45 -0700

    video 2 done

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+set_time_limit(0);
+ini_set('default_socket_timeout', 300);
+session_start();
+define('client_id',  'c73d173254d844b89d8117954f97d9ee');
+define('client_secret',  'c73d173254d844b89d8117954f97d9ee');
+define('redirectURL',  'http://localhost/appacademyapi/index.php');
+define('ImageDirectory',  'pics/');
+?>
+
+
+<!-- CLIENT INFO
+CLIENT ID c73d173254d844b89d8117954f97d9ee
+CLIENT SECRET 971766cd8c4f4af7b7a6ff36f32b68b0
+WEBSITE URL http://localhost/appacademyapi/index.php
+REDIRECT URL http://localhost/appacademyapi/index.php -->
